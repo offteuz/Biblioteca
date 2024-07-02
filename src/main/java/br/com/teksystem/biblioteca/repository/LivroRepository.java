aa1dd5c06bd9f9a5ad6a1cee0c6608c466fbddd2
Matheus Jesus de Souza <matheusxone47@gmail.com>
2024-07-02 06:03:59 -0300

    Criação de meios de busca no Repository

--- src/main/java/br/com/teksystem/biblioteca/repository/LivroRepository.java
+++ src/main/java/br/com/teksystem/biblioteca/repository/LivroRepository.java
@@ -5,4 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LivroRepository extends JpaRepository<Livro, Long> {
 
+    public Livro findByTitulo(String titulo);
+    public Livro findByAutor(String autor);
+    public Livro findByCategoria(String categoria);
+
 }
